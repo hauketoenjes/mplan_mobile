6715c75382fa409f2583ce48fec6fd0dfd0e1577
Hauke Tönjes <git@htoenjes.de>
2023-10-29 14:55:35 +0100

    feat: Add empty list info, change API URL (#33)

--- lib/app_configuration.dart
+++ lib/app_configuration.dart
@@ -6,5 +6,5 @@ Uri planUrl = Uri.parse('https://messdiener-visbek.de/plan');
 
 /// The URL to the plan fetch function
 Uri planFetchUrl = Uri.parse(
-  'https://europe-west3-messdienerplan-984ef.cloudfunctions.net/getPlan',
+  'https://mplan.messdiener-visbek.de/api/plan',
 );
